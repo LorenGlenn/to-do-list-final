c1dafb45c6501e0b6968a2971764886a9f7fb01a
Steve Burton and taylor loftis-kim <steve+taylor@>
2016-11-16 11:12:23 -0800

    Add id tags to to do list

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using Nancy;
+using ToDoList.Objects;
+
+namespace ToDoList
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+      Get["/tasks"] = _ => {
+        List<Task> allTasks = Task.GetAll();
+        return View["tasks.cshtml", allTasks];
+      };
+      Get["/tasks/new"] = _ => {
+        return View["task_form.cshtml"];
+      };
+      Get["tasks/{id}"] = parameters => {
+        Task task = Task.Find(parameters.id);
+        return View["/task.cshtml", task];
+      };
+      Post["/tasks"] = _ => {
+        Task newTask = new Task(Request.Form["new-task"]);
+        List<Task> allTasks = Task.GetAll();
+        return View["tasks.cshtml", allTasks];
+      };
+    }
+  }
+}
